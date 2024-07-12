0a3f0346ca4903e24f239abc43aebd077e081bd8
Andrej <amilanovi0@gmail.com>
2024-07-12 19:46:08 +0200

    Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navyBlue: '#001F3F',
+        lightGray: '#DDDDDD',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
