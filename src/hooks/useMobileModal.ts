8602a0a8dac421af483b3edaaa7a022a3723e7ce
Andrej <amilanovi0@gmail.com>
2024-07-15 15:37:09 +0200

    Close modal on route change

--- src/hooks/useMobileModal.ts
+++ src/hooks/useMobileModal.ts
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function useMobileModal() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   function openModal() {
     setIsOpen(true);
@@ -10,6 +14,10 @@ export default function useMobileModal() {
   function closeModal() {
     setIsOpen(false);
   }
+
+  useEffect(() => {
+    closeModal();
+  }, [pathname, searchParams]);
 
   return { isOpen, openModal, closeModal };
 }
