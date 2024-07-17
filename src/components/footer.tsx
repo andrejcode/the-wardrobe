0bd76bead0df1c6077d131c45bb4d9a1cbad2a0e
Andrej <amilanovi0@gmail.com>
2024-07-17 17:14:21 +0200

    Add Privacy Policy, Terms and Conditions and Return Policy pages with each having PlaceholderPage component. In the footer add links to those pages.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,25 @@
+import Link from 'next/link';
+
 export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer>
+      <hr className="border-lightGray" />
+      <ul className="mx-auto my-6 w-1/2 md:mx-4 md:w-full">
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/terms-conditions">
+          <li>Terms & Conditions</li>
+        </Link>
+        <Link href="/shipping-return-policy">
+          <li>Shipping & Return Policy</li>
+        </Link>
+        <Link href="/privacy-policy">
+          <li>Privacy Policy</li>
+        </Link>
+      </ul>
       <hr className="border-lightGray" />
       <p className="my-4 text-center text-sm md:text-lg">
         <a href="https://andrejmilanovic.com">
---
+++ src/components/placeholder-page.tsx
@@ -0,0 +1,19 @@
+export default function PlaceholderPage({ title }: { title: string }) {
+  return (
+    <section className="flex h-screen flex-col items-center justify-center">
+      <div className="max-w-md text-center">
+        <h1 className="mb-2 text-left text-xl font-bold">{title}</h1>
+        <p className="text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucius nisl.
+          Quisque id diam vel quam elementum. Eros donec ac odio tempor orci
+          dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque eu
+          tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </div>
+    </section>
+  );
+}
