3c606c6d75a582d46c2031dc1afcf91b11e5a22a
Andrej <amilanovi0@gmail.com>
2024-07-18 14:38:47 +0200

    Add logo to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Logo from './header/logo';
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -6,20 +7,25 @@ export default function Footer() {
   return (
     <footer>
       <hr className="border-lightGray" />
-      <ul className="mx-auto my-6 w-1/2 md:mx-4 md:w-full">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/terms-conditions">Terms & Conditions</Link>
-        </li>
-        <li>
-          <Link href="/shipping-return-policy">Shipping & Return Policy</Link>
-        </li>
-        <li>
-          <Link href="/privacy-policy">Privacy Policy</Link>
-        </li>
-      </ul>
+
+      <div className="mx-auto my-6 flex w-1/2 flex-col items-start md:mx-4 md:w-full md:flex-row">
+        <Logo />
+        <ul className="md:ml-6 md:py-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/terms-conditions">Terms & Conditions</Link>
+          </li>
+          <li>
+            <Link href="/shipping-return-policy">Shipping & Return Policy</Link>
+          </li>
+          <li>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+          </li>
+        </ul>
+      </div>
+
       <hr className="border-lightGray" />
       <p className="my-4 text-center text-sm md:text-lg">
         <a href="https://andrejmilanovic.com">
