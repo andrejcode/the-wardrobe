4cda774e750c5ea8c954dbe4030b91aba325e20d
Andrej <amilanovi0@gmail.com>
2024-07-18 14:22:16 +0200

    State of the menu is handled inside the component now. Icon is also added to the component. This is done mainly so that header does not need to be Client Component.

--- src/components/header/mobile-menu.tsx
+++ src/components/header/mobile-menu.tsx
@@ -1,14 +1,30 @@
+'use client';
+
 import Search from './search';
 import NavLinks from './nav-links';
 import MobileModal from '../mobile-modal';
+import { IoMenuOutline } from 'react-icons/io5';
+import useMobileModal from '@/hooks/useMobileModal';
 
-export default function MobileMenu({ closeMenu }: { closeMenu: () => void }) {
+export default function MobileMenu() {
+  const { isOpen, openModal, closeModal } = useMobileModal();
+
   return (
-    <MobileModal closeModal={closeMenu}>
-      <>
-        <Search placeholder="Search for items" />
-        <NavLinks />
-      </>
-    </MobileModal>
+    <>
+      <IoMenuOutline
+        size={24}
+        className="mr-4 cursor-pointer"
+        onClick={openModal}
+      />
+      {isOpen && (
+        <MobileModal closeModal={closeModal}>
+          <>
+            <Search placeholder="Search for items" />
+            <div className="mt-4"></div>
+            <NavLinks />
+          </>
+        </MobileModal>
+      )}
+    </>
   );
 }
