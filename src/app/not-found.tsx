5458304a30cd3886aced61e94fe8b066a969e35f
Andrej <amilanovi0@gmail.com>
2024-07-18 09:47:14 +0200

    Remove classname from Link and add bottom margin to the p element

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
   return (
     <section className="flex h-screen flex-col items-center justify-center">
       <h2 className="mb-3 text-center text-3xl md:text-4xl">404 Not Found</h2>
-      <p className="text-center text-lg">
+      <p className="mb-4 text-center text-lg">
         The page you are looking for does not exist.
       </p>
-      <Link href="/" className="mt-4">
+      <Link href="/">
         <Button>Return Home</Button>
       </Link>
     </section>
