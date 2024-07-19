c44534a204aa0ffc97972d03052889e52076a15e
Andrej <amilanovi0@gmail.com>
2024-07-19 20:02:38 +0200

    Remove margin right

--- src/components/header/icons/signin-icon.tsx
+++ src/components/header/icons/signin-icon.tsx
@@ -4,11 +4,7 @@ import { IoLogInOutline } from 'react-icons/io5';
 export default function SignInIcon() {
   return (
     <Link href="/signin">
-      <IoLogInOutline
-        className="mr-4 cursor-pointer"
-        size={24}
-        title="Sign in"
-      />
+      <IoLogInOutline className="cursor-pointer" size={24} title="Sign in" />
     </Link>
   );
 }
