83ab554d62644a8482e6d0dbfcd655d1a25c6ed9
Andrej <amilanovi0@gmail.com>
2024-07-22 12:16:50 +0200

    Wrap search in suspense

--- src/components/header/search.tsx
+++ src/components/header/search.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import { IoIosSearch } from 'react-icons/io';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -22,23 +22,25 @@ export default function Search({ placeholder }: { placeholder: string }) {
   }
 
   return (
-    <div className="relative mx-4">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        id="search"
-        className="block w-full rounded-md py-2 pl-10 text-sm text-black placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(event) => setTerm(event.target.value)}
-        defaultValue={searchParams.get('q')?.toString()}
-        onKeyDown={handleSearch}
-      />
-      <IoIosSearch
-        size={24}
-        color="gray"
-        className="absolute left-3 top-1/2 -translate-y-1/2"
-      />
-    </div>
+    <Suspense>
+      <div className="relative mx-4">
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
+        <input
+          id="search"
+          className="block w-full rounded-md py-2 pl-10 text-sm text-black placeholder:text-gray-500"
+          placeholder={placeholder}
+          onChange={(event) => setTerm(event.target.value)}
+          defaultValue={searchParams.get('q')?.toString()}
+          onKeyDown={handleSearch}
+        />
+        <IoIosSearch
+          size={24}
+          color="gray"
+          className="absolute left-3 top-1/2 -translate-y-1/2"
+        />
+      </div>
+    </Suspense>
   );
 }
